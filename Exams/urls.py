b37540b91b80823c434170a185451136b7ac721a
Alireza <sahseyyedalirezahosseini1384@gmail.com>
2026-05-31 14:34:40 +0330

    added edit-exam url

--- Exams/urls.py
+++ Exams/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('exit-exam/', views.exit_exam, name='exit_exam'),
     path('delete-exam/<int:course_id>/', views.delete_exam, name='delete_exam'),
     path('create-exam/<int:course_id>', views.create_exam, name='create_exam'),
+    path('edit-exam/<int:course_id>/<int:exam_id>', views.edit_exam, name='edit_exam'),
     path('question/<int:exam_id>/<int:course_id>/', views.question_page, name='question_page'),
 ]
